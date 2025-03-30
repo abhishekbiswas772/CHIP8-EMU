a676ffb73711087f2ea4f32fa38582b520b8b2ce
Abhishek Biswas <abhishek.biswas@cubastion.com>
2025-03-31 01:36:38 +0530

    stack ptr fix

--- include/chip8.h
+++ include/chip8.h
@@ -1,14 +1,16 @@
 #ifndef CHIP8_H
-#define CHIP8_h
+#define CHIP8_H
 
 #include "chip8memory.h"
 #include "config.h"
 #include "chip8register.h"
+#include "chip8stack.h"
 
 struct chip8
 {
     struct chip8_memory memory;
     struct chip8_registers registers;
+    struct chip8_stack stack;
 
 };
 
