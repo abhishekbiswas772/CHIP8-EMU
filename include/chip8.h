f465cd0b9cafd4f3330ade4867d7aa644f3391c2
Abhishek Biswas <abhishek.biswas@cubastion.com>
2025-03-31 00:13:57 +0530

    fix

---
+++ include/chip8.h
@@ -0,0 +1,14 @@
+#ifndef CHIP8_H
+#define CHIP8_h
+
+#include "chip8memory.h"
+#include "config.h"
+
+struct chip8
+{
+    struct chip8_memory memory;
+
+};
+
+
+#endif
--- src/main.c
+++ src/main.c
@@ -2,14 +2,19 @@
 #include <stdbool.h>
 #include <stdlib.h> 
 #include "SDL2/SDL.h"
+#include "chip8.h"
 
 int main(int argc, char** argv){
+    struct chip8 chip8;
+    chip8_memory_set(&chip8.memory, 0x400, 'Z');
+    printf("%c\n", chip8_memory_get(&chip8.memory, 50));
+
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window* window = SDL_CreateWindow(
-        "Chip8 Emulator", 
+        EMULATOR_WINDOW_TITLE, 
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED, 
-        640, 320, 
+        CHIP8_WIDTH * CHIP8_WINDOW_MULTIPLIER, CHIP8_HEIGHT * CHIP8_WINDOW_MULTIPLIER, 
         SDL_WINDOW_SHOWN
     );
     SDL_Renderer*  renderer = SDL_CreateRenderer(window, -1, SDL_TEXTUREACCESS_TARGET);
